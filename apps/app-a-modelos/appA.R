91c6328e854136af391679f1ef205ccedd5c4834
MoisesTH <62824374+MoisesTH@users.noreply.github.com>
2026-07-13 02:02:05 -0600

    Add Shiny app structure with necessary libraries

--- apps/app-a-modelos/appA.R
+++ apps/app-a-modelos/appA.R
@@ -1,1 +1,22 @@
+library(shiny)
+library(ggplot2)
 
+source("R/helpers/app_config.R")
+source("R/helpers/input_validation.R")
+source("R/helpers/formatting.R")
+
+source("R/shared/discount_models.R")
+source("R/shared/utility_functions.R")
+source("R/shared/ctb_functions.R")
+source("R/shared/choice_functions.R")
+source("R/shared/visualization_functions.R")
+
+source("R/modules/mod_discount_curves.R")
+source("R/modules/mod_binary_choice.R")
+source("R/modules/mod_ctb_model.R")
+source("R/modules/mod_model_components.R")
+
+ui <- navbarPage(...)
+server <- function(input, output, session) {...}
+
+shinyApp(ui, server)
